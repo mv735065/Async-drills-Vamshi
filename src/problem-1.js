3bb5f1666a7cb362111d9aed55c72b482e17205f
mv735065 <245119735065@mvsrec.edu.in>
2025-02-19 14:12:44 +0530

    completed fs drill using callbacks

---
+++ src/problem-1.js
@@ -0,0 +1,81 @@
+// Problem 1:
+
+// Using callbacks and the fs module's asynchronous functions, do the following:
+//     1. Create a directory of random JSON files
+//     2. Delete those files simultaneously
+
+const fs = require("fs");
+const path = require("path");
+
+const dir = path.join(__dirname, "randomFile");
+let create = 15;
+
+function createFiles(callbackError) {
+  fs.mkdir(dir, (err) => {
+    if (err) return callbackError(err);
+
+    let filesCreated = 0;
+    for (let i = 1; i <= create; i++) {
+      let file = path.join(dir, `file-${i}.json`);
+      let randomData = {
+        name: file,
+        value: i,
+      };
+
+      fs.writeFile(file, JSON.stringify(randomData, null, 2), (err) => {
+        if (err) return callbackError(err);
+
+        filesCreated++;
+        console.log("file is created:",filesCreated);
+        if (filesCreated === create) {
+          callbackError(null);
+        }
+      });
+    }
+  });
+}
+
+function deleteFiles(callbackError) {
+  fs.readdir(dir, (err, files) => {
+    if (err) return callbackError(err);
+
+    if (files.length === 0) {
+      return callbackError("Directory is empty");
+    }
+
+    let filesDeleted = 0;
+    files.forEach((file) => {
+      fs.rm(path.join(dir, file), (err) => {
+        if (err) return callbackError(err);
+        filesDeleted++;
+        console.log("file is deleted:",filesDeleted);
+
+        if (filesDeleted === files.length) {
+          fs.rmdir(dir, (err) => {
+            if (err) return callbackError(err);
+            callbackError(null);
+          });
+        }
+      });
+    });
+  });
+}
+
+
+// module.exports={createFiles,deleteFiles};
+
+createFiles((err) => {
+  if (err) {
+    console.log("Error creating files:", err);
+  } else {
+    console.log("Created files successfully!");
+
+    deleteFiles((err) => {
+      if (err) {
+        console.log("Error deleting files:", err);
+      } else {
+        console.log("Deleted files and directory successfully!");
+      }
+    });
+  }
+});
---
+++ src/problem-2.js
@@ -0,0 +1,145 @@
+/*
+    Problem 2:
+    
+    Using callbacks and the fs module's asynchronous functions, do the following:
+        1. Read the given file lipsum.txt
+        2. Convert the content to uppercase & write to a new file. Store the name of the new file in filenames.txt
+        3. Read the new file and convert it to lower case. Then split the contents into sentences. Then write it to a new file. Store the name of the new file in filenames.txt
+        4. Read the new files, sort the content, write it out to a new file. Store the name of the new file in filenames.txt
+        5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
+*/
+
+const fs = require("fs");
+const path = require("path");
+let dirPath = path.resolve();
+dirPath = path.join(dirPath, "Data-texts");
+
+let storeFilesNames = path.join(dirPath, "filenames.txt");
+
+readTheFile((err, data) => {
+  if (err) {
+    console.error("Error in reading file lipsum:", err);
+  } else {
+    console.log("Successfully read the file");
+
+    convertUpperCase(data, (err, upperCaseData) => {
+      if (err) {
+        console.error("error converting text to Uppsercase:", err);
+      } else {
+        console.log("Converted the text to Upppsercase");
+
+        covertDataToLowerCaseAndSplit(upperCaseData, (err, lowerData) => {
+          if (err) {
+            console.error("error converting text to Lowercase:", err);
+          } else {
+            console.log("Converted the text to Lowercase and splitted");
+
+            sortTheData(lowerData, (err, sortedData) => {
+              if (err) {
+                console.error("error in spllited data:", err);
+              } else {
+                console.log("Succesfully data is sorted");
+
+                deleteFilesInList((err) => {
+                  if (err) console.error("error in files not deleted :", err);
+                  else {
+                    console.log("Files Successfully deleted");
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+function readTheFile(callback) {
+  let filePath = path.join(dirPath, "lipsum.txt");
+  fs.readFile(filePath, "utf-8", callback);
+}
+
+function convertUpperCase(data, callback) {
+  let newFile = path.join(dirPath, "/uppserCaseDataFile.txt");
+  let res = data.toUpperCase();
+  fs.writeFile(newFile, res, "utf-8", (err) => {
+    if (err) {
+      return callback(err);
+    } else {
+      fs.appendFile(storeFilesNames, "uppserCaseDataFile.txt\n", (err) => {
+        if (err) console.log("Unable to add file to filenames: ", err);
+        else callback(null, res);
+      });
+    }
+  });
+}
+
+function covertDataToLowerCaseAndSplit(upperData, callback) {
+  let newFile = path.join(dirPath, "/lowerCaseDataFile.txt");
+  let res = upperData.toLowerCase();
+  let splited = res.split(".").join("\n");
+
+  fs.writeFile(newFile, splited, "utf-8", (err) => {
+    if (err) {
+      return callback(err);
+    } else {
+      fs.appendFile(storeFilesNames, "lowerCaseDataFile.txt\n", (err) => {
+        if (err) console.log("Unable to add file to filenames: ", err);
+        else callback(null, splited);
+      });
+    }
+  });
+}
+
+function sortTheData(data, callback) {
+  let newFile = path.join(dirPath, "sortedDataFile.txt");
+  let res = data.toLowerCase();
+  let sorted = res.split("\n").sort().join("\n");
+
+  fs.writeFile(newFile, sorted, "utf-8", (err) => {
+    if (err) {
+      return callback(err);
+    } else {
+      fs.appendFile(storeFilesNames, "sortedDataFile.txt\n", (err) => {
+        if (err) console.log("Unable to add file to filenames: ", err);
+        else callback(null, sorted);
+      });
+    }
+  });
+}
+
+function deleteFilesInList(callback) {
+  let fileName = storeFilesNames;
+
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) return callback(err);
+    let files = data
+      .split("\n")
+      .map((file) => file.trim())
+      .filter(Boolean);
+    console.log(files);
+
+    if (files.length === 0) {
+      return callback("No files to delete");
+    }
+    let deletedCount = 0;
+
+    files.forEach((file, index) => {
+      let name=file;
+      fs.rm(path.join(dirPath, `${file}`), { force: true }, (err) => {
+        if (err) return callback(err);
+        deletedCount++;
+        console.log(name);
+        
+
+        if (deletedCount === files.length) {
+          fs.writeFile(storeFilesNames, "", (err) => {
+            if (err) return callback(err);
+            else callback(null);
+          });
+        }
+      });
+    });
+  });
+}
